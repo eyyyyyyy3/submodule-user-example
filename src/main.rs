59648de14444a80a72fd590d476b3729a615838f
eyyyyyyy3@gmail.com <eyyyyyyy3@gmail.com>
2024-11-22 11:11:50 +0100

    feat: implement gRPC server in main.rs and update submodule

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use proto::example_server::{Example, ExampleServer};
+use tonic::{transport::Server, Response};
+
+mod proto {
+    tonic::include_proto!("example.v1");
+}
+
+#[derive(Default, Debug)]
+struct ExampleService {}
+
+#[tonic::async_trait]
+impl Example for ExampleService {
+    async fn say_something(
+        &self,
+        _request: tonic::Request<proto::SaySomethingRequest>,
+    ) -> Result<tonic::Response<proto::SaySomethingResponse>, tonic::Status> {
+        let response = proto::SaySomethingResponse {
+            answer: "HELLO FROM THE SERVER".to_owned(),
+        };
+
+        Ok(tonic::Response::new(response))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let addr = "127.0.0.1:4000".parse()?;
+
+    let example_service = ExampleService::default();
+    let example_server = ExampleServer::new(example_service);
+
+    Server::builder()
+        .add_service(example_server)
+        .serve(addr)
+        .await?;
+    Ok(())
 }
